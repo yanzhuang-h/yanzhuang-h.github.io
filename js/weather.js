8d32787709ed9b4cc7120ee4ad06f3ab909eaf18
yanzhuang-h <2817199754@qq.com>
2023-08-19 23:41:30 +0800

    Site updated: 2023-08-19 23:41:29

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FF9900",
+    "tmpSize": "12",
+    "cityColor": "#46798b",
+    "citySize": "12",
+    "aqiColor": "#46798b",
+    "aqiSize": "12",
+    "weatherIconSize": "18",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "140",
+    "top": "12",
+    "key": "f3ee69d63d3b47b2bf4cfe2dbef3093a"
+  }
+}
